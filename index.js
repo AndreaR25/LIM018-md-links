1492e2cbff942699a1ec66236f5646688740fbba
Andrea Reyes Vergara <andreareyes@Andreas-MacBook-Air.local>
2022-09-19 23:21:11 -0500

    verificacion de ruta

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const fs = require("fs")
+const path = require("path")
+
+const existRuta = (path) => fs.existsSync(path)
+
+console.log(existRuta(/Users/andreareyes/Documents/LIM018-md-links/test/md-links.spec.js))
